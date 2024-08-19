fa9faca561f427f7e36e298f1c6489085b0f92c0
priest-2105 <fawzybailey782@gmail.com>
2024-08-19 03:52:26 +0100

    commit

--- super.js
+++ super.js
@@ -1,18 +1,65 @@
+const copyclipboard = document.getElementById('copyclipboard');
+const copyclipboardcheck = document.getElementById('copyclipboardcheck');
+
+
+copyclipboard.style.display = 'none';
+copyclipboardcheck.style.display = 'none';
+
+
 const generatePasswordButton = document.getElementById('generatePassword');
+const passwordStrength = document.getElementById('passwordStrength');
+const passwordResult = document.getElementById('passwordResult');
+
+
+
+function generatePassword(length, charset) {
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
 
 generatePasswordButton.addEventListener('click', () => {
-    const passwordLength = 12;
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
-    let securePassword = '';
 
-    for (let i = 0; i < passwordLength; i++) {
-        securePassword += characters.charAt(Math.floor(Math.random() * characters.length));
+    copyclipboard.style.display = 'block';
+
+    const selectedStrength = passwordStrength.value;
+    let password = '';
+    
+    if (selectedStrength === 'weak') {
+        password = generatePassword(6, 'abcdefghijklmnopqrstuvwxyz');
+    } else if (selectedStrength === 'normal') {
+        password = generatePassword(8, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    } else if (selectedStrength === 'strong') {
+        password = generatePassword(12, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
+    } else if (selectedStrength === 'veryStrong') {
+        password = generatePassword(16, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+');
     }
-
-
+    
+    passwordResult.value = password;
+    console.log(`Generated ${selectedStrength} password: ${password}`);
+});
     
 
-    document.getElementById('passwordResult').value = securePassword;
 
-    console.log(securePassword);
-});
+
+
+copyclipboard.addEventListener('click', () => {
+    const password = document.getElementById('passwordResult').value;
+    navigator.clipboard.writeText(password)
+       .then(() => {
+            console.log('Password copied to clipboard');
+            copyclipboardcheck.style.display = 'block';
+            setTimeout(() => {
+                copyclipboardcheck.style.display = 'none';
+            }, 1000);
+        })
+       .catch(err => {
+            console.error('Failed to copy password to clipboard:', err);
+        });
+    copyclipboard.style.display = 'none';
+    copyclipboardcheck.style.display = 'block';
+
+})
